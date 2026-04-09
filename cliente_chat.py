8f80954bde24b56875a7ea42ec4b3f18cc8c0259
Cristobal Alfaro <cristobal.alfarok@userena.cl>
2026-04-08 20:49:24 -0400

    Se hicieron cambios en el README, se agrego mensajes de bienvenida para el usuario en cliente_chat.py, un mensaje adicional para salir y un help! , se añadio la guia del taller, se asigno la ip a LOCALHOST  en server2.py

--- cliente_chat.py
+++ cliente_chat.py
@@ -13,10 +13,12 @@ cliente_socket.connect(('localhost', 8000))
 # Iniciar el hilo para recibir mensajes
 hilo_recibir = threading.Thread(target=recibir_mensajes, args=(cliente_socket,))
 hilo_recibir.start()
-print ("<<Bienvenidos al Chat de Sistemas operativo>>\n")
+print ("\n<<Bienvenido al Chat de Sistemas operativos>>\n\n Aqui puedes escribir tus mensajes para comunicarte con los demas usuarios conectados al chat!\n\n Escribe 'help!' para obtener ayuda sobre los comandos disponibles.\n")
 while True:
     mensaje = input(">")
     cliente_socket.send(mensaje.encode("utf-8"))
-    if mensaje=="exit":
+    if mensaje=="help!":
+        print("para salir del chat solo escribe 'exit' o 'salir'")
+    if mensaje=="exit" or mensaje=="salir":
         break
 print ("Has salido del chat...")
